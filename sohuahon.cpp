6cad80188136161c4c65f5a1a4f7724399b011d7
duytien241 <lucthuyvip@gmail.com>
2017-11-25 06:10:53 +0700

    Update sohuahon.cpp

--- sohuahon.cpp
+++ sohuahon.cpp
@@ -2,14 +2,12 @@
 #include <conio.h>
 #define MAX 999999
 int a[MAX][1];
-bool b[MAX];
 int main(){
 	int x;
 	do{
 		scanf("%d",&x);
 	}while(x<=0);
     for(int i=1;i<=x;i++){
-    	b[i]=1;
     	int ti=0;
     	for(int t=1;t<i;t++){
     		if(i%t==0) ti+=t;
@@ -17,10 +15,9 @@ int main(){
     	a[i][0]=ti-1;
     }
     for(int i=1;i<x;i++){
-    	for(int t=0;t<x;t++){
-    		if(a[i][0]==t&&a[t][0]==i&&b[i]){
+    	for(int t=i;t<x;t++){
+    		if(a[i][0]==t&&a[t][0]==i){
     			printf("%d, %d\n",i,t);
-    			b[t]=0;
     			break;
     		} 
     	}
