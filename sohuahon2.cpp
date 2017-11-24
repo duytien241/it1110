b46f5d97b3c47af971558372faaf6275e032803a
duytien241 <lucthuyvip@gmail.com>
2017-11-25 06:46:13 +0700

    Update sohuahon2.cpp

--- sohuahon2.cpp
+++ sohuahon2.cpp
@@ -5,16 +5,18 @@ long a[MAX][1];
 int main(){
 	int x;
 	do{
+		printf("Nhap x:");
 		scanf("%d",&x);
 	}while(x<=0);
-    for(int i=1;i<=x;i++){
+	printf("Cac so hua hon la:\n");
+        for(int i=1;i<=x;i++){
     	int tiendz=0; // :))
     	for(int t=1;t<i;t++){
     		if(i%t==0) tiendz+=t;
     	}
     	a[i][0]=tiendz-1;
         if(tiendz<i&&i==a[tiendz-1][0]) printf("%d,%d\n",tiendz-1,i);
-    }
-    getch();
+        }
+        getch();
 	return 0;
 }
