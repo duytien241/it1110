f07ff29c4b08b91cec75438738c85605758405d4
duytien241 <lucthuyvip@gmail.com>
2017-11-25 06:53:34 +0700

    Update sohuahon2.cpp

--- sohuahon2.cpp
+++ sohuahon2.cpp
@@ -3,20 +3,26 @@
 #define MAX 99999999
 long a[MAX][1];
 int main(){
-	int x;
-	do{
-		printf("Nhap x:");
-		scanf("%d",&x);
-	}while(x<=0);
-	printf("Cac so hua hon la:\n");
+    int x;
+    int s=0;
+    do{
+        printf("Nhap x:");
+        scanf("%d",&x);
+    }while(x<=0);
         for(int i=1;i<=x;i++){
-    	int tiendz=0; // :))
-    	for(int t=1;t<i;t++){
-    		if(i%t==0) tiendz+=t;
-    	}
-    	a[i][0]=tiendz-1;
-        if(tiendz<i&&i==a[tiendz-1][0]) printf("%d,%d\n",tiendz-1,i);
+        int tiendz=0; // :))
+        for(int t=1;t<i;t++){
+            if(i%t==0) tiendz+=t;
         }
-        getch();
-	return 0;
+        a[i][0]=tiendz-1;
+        if(tiendz<i&&i==a[tiendz-1][0]) {
+            if(s==0) printf("Cac so hua hon la:\n");
+            s++;
+            printf("%d,%d\n",tiendz-1,i);
+        }
+    }
+    if(s==0) printf("Khong co so nao.");
+    else printf("End.");
+    getch();
+    return 0;
 }
